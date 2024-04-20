2d088827a9795af3e57907531282551dde0dff58
Enes Akkal <enesakkal48@gmail.com>
2024-04-20 19:58:26 +0300

    file name changed

--- React/src/App.js
+++ React/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginFormPageComponent from 'components/LoginFormPageComponent.js';
+import LoginFormPageComponent from './components/LoginFormPageComponent.js';
 import HomePageComponent from './components/HomePageComponent.js';
-import UserProfile from 'components/UserProfile.js';
-import Post from 'components/Post.js';
-import ProfileComponent from 'components/ProfileComponent.js';
+import UserProfile from './components/UserProfile.js';
+import Post from './components/Post.js';
+import ProfileComponent from './components/ProfileComponent.js';
 
 function App() {
   return (
