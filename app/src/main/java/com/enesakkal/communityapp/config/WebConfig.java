97bc3863aa7fce0049dbe3e8deb619a62e4c3ea2
Enes Akkal <enesakkal48@gmail.com>
2024-05-21 20:02:57 +0300

    update webconfig

--- app/src/main/java/com/enesakkal/communityapp/config/WebConfig.java
+++ app/src/main/java/com/enesakkal/communityapp/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://35.209.224.209:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type", "Accept")
                 .allowCredentials(true)
